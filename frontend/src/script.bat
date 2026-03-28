6572dd81d92c73f0c104d4302d344772da6edb9f
zohafaiyazhaei-code <zoha.faiyaz.haei@gmail.com>
2026-03-28 22:32:13 +0530

    dealer list ui now consistent w the others

--- frontend/src/script.bat
+++ frontend/src/script.bat
@@ -1,11 +1,20 @@
-@echo off
+REM@echo off
 rem Save everything directly to output.txt without echoing to console
+REM(
+REM    tree /f
+REM    for /r %%f in (*) do (
+REM        if /i not "%%~xf"==".css" if /i not "%%~xf"==".html" (
+REM            echo ==== %%f ====
+REM            type "%%f"
+REM        )
+REM    )
+REM) > output.txt
+
+@echo off
 (
     tree /f
-    for /r %%f in (*) do (
-        if /i not "%%~xf"==".css" if /i not "%%~xf"==".html" (
-            echo ==== %%f ====
-            type "%%f"
-        )
+    for /r %%f in (*.css *.html) do (
+        echo ==== %%f ====
+        type "%%f"
     )
 ) > output.txt
